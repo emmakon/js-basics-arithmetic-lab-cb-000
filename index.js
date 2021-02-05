63afc23d63233ca7cf611ebbd37ca3ea98e3132d
Emma Konrad <emma.konrad92@gmail.com>
2021-02-05 00:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 
 let multiply = var1*var2;
-const random = Math.random() * 10;
+const random = Math.random() * 20;
