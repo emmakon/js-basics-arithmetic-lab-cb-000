020d7625f87a4b6724ea8fa4d5ea5a4b71d54041
Emma Konrad <emma.konrad92@gmail.com>
2021-02-05 00:06:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,3 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-const multiply = var1*var2;
-const random = Math.floor(Math.random() * 10 + 10);
-const mod = 29 % 5;
-const max = Math.max(1, 4, 7, 9, 20);
